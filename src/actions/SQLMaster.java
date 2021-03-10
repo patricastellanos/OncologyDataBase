153f8378d31267dd6797fd851de5b7fa10d5d7fc
maria <maria@10.60.59.102>
2021-03-10 19:42:06 +0100

    Connect, disconnect and tables of the data base created :)

--- src/actions/SQLMaster.java
+++ src/actions/SQLMaster.java
@@ -3,6 +3,7 @@ package actions;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 import oncology.db.interfaces.DBMaster;
@@ -10,47 +11,108 @@ import oncology.db.pojos.Cancer;
 import oncology.db.pojos.Patient;
 
 public class SQLMaster implements DBMaster {
-	
-  private Connection c;
+
+	private Connection c;
 
 	@Override
 	public void connect() {
-			try {
-				// Open database connection
-				Class.forName("org.sqlite.JDBC");
-				c = DriverManager.getConnection("jdbc:sqlite:./db/jobSeeker.db");
-				c.createStatement().execute("PRAGMA foreign_keys=ON");
-				System.out.println("Database connection opened.");
-				this.createTables();
-			} catch (SQLException sqlE) {
-				System.out.println("There was a database exception.");
-				sqlE.printStackTrace();
-			} catch (Exception e) {
-				System.out.println("There was a general exception.");
-				e.printStackTrace();
-			}
-		
+		try {
+			// Open database connection
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:./db/oncologyDB");
+			c.createStatement().execute("PRAGMA foreign_keys=ON");
+			System.out.println("Database connection opened.");
+			this.createTables();
+		} catch (SQLException sqlE) {
+			System.out.println("There was a database exception.");
+			sqlE.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("There was a general exception.");
+			e.printStackTrace();
+		}
 
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void disconnect() {
-		// TODO Auto-generated method stub
-		
+		try {
+			// Close database connection
+			c.close();
+		} catch (SQLException e) {
+			System.out.println("There was a problem while closing the database connection.");
+			e.printStackTrace();
+		}
+	}
+
+	private void createTables() {
+		// If the tables are not created already, create them
+		Statement stmt1;
+		try {
+			stmt1 = c.createStatement();
+			// Create table patient
+			String sql1 = "CREATE TABLE patient "
+					+ "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
+					+ " name     TEXT     NOT NULL, "
+					+ " surname     TEXT     NOT NULL, "
+					+ " sex TEXT NOT NULL, "
+					+ " date_birth DATE NOT NULL, " 
+					+ " location TEXT, " 
+					+ " actual_state NOT NULL, "
+					+ " id_famHistory INTEGER REFERENCES family_history (id_famHistory) ON DELETE SET NULL, )";
+			stmt1.executeUpdate(sql1);
+			// Create table cancer
+			sql1 = "CREATE TABLE cancer " 
+					+ "( id_cancer INTEGER PRIMARY KEY AUTOINCREMENT, "
+					+ " id_medExam REFERENCES medical_examinations (id_medExam) ON DELETE SET NULL, "
+					+ " type TEXT NOT NULL, )";
+
+			stmt1.executeUpdate(sql1);
+			// Create table cancer_treatment
+			sql1 = "CREATE TABLE cancer_treatment " 
+					+ "( id_cancer INTEGER REFERENCES cancer (id_cancer), "
+					+ "id_treat INTEGER REFERENCES treatment (id_treatment), " 
+					+ "PRIMARY KEY (id_cancer, id_treat, )";
+			stmt1.executeUpdate(sql1);
+
+			sql1 = "CREATE TABLE symptomps " 
+					+ "(id_symp  INTEGER  PRIMARY KEY AUTOINCREMENT,"
+					+ " detail    TEXT     NOT NULL, )";
+			stmt1.executeUpdate(sql1);
+
+			sql1 = "CREATE TABLE treatment " + "(id_treat   INTEGER  PRIMARY KEY AUTOINCREMENT,"
+					+ " type    TEXT     NOT NULL, "
+					+ " startdate     DATE NOT NULL, "
+					+ " enddate DATE NOT NULL  TEXT, )";
+			stmt1.executeUpdate(sql1);
+			stmt1.close();
+
+			sql1 = "CREATE TABLE patient_symptomps"
+					+ "(id_patient INTEGER REFERENCES patient(id_patient) ON DELETE SET NULL, "
+					+ " id_symp INTEGER REFERENCES symptomps(id_symp) ON DELETE SET NULL, "
+					+ "PRIMARY KEY (id_patient, id_symp))";
+
+			stmt1.executeUpdate(sql1);
+			stmt1.close();
+		} catch (SQLException e) {
+			if (!e.getMessage().contains("already exists")) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 
 	@Override
 	public void addPatient(Patient p) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void removePatient(Patient p) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
