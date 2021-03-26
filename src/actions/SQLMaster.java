1b1feebed7baeefd8ae64457de65e4574e9644c2
patri <patri@10.60.58.63>
2021-03-26 20:21:20 +0100

    Mistakes in tables changed

--- src/actions/SQLMaster.java
+++ src/actions/SQLMaster.java
@@ -2,6 +2,7 @@ package actions;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -64,9 +65,9 @@ public class SQLMaster implements DBMaster {
 					+ " surname     TEXT     NOT NULL, "
 					+ " sex TEXT NOT NULL, "
 					+ " date_birth DATE NOT NULL, " 
-					+ " location TEXT, " 
-					+ " actual_state NOT NULL, "
-					+ " id_famHistory INTEGER REFERENCES family_history (id_famHistory) ON DELETE SET NULL)";
+					+ " location TEXT NOT NULL, " 
+					+ " actual_state TEXT NOT NULL, "
+					+ " id_famHistory INTEGER REFERENCES family_history (id_famHistory) ON DELETE SET NULL )";
 			stmt1.executeUpdate(sql1);
 			
 			// Create table cancer
@@ -81,13 +82,13 @@ public class SQLMaster implements DBMaster {
 			sql1 = "CREATE TABLE cancer_treatment " 
 					+ "( id_cancer INTEGER REFERENCES cancer (id_cancer), "
 					+ " id_treat INTEGER REFERENCES treatment (id_treatment), " 
-					+ " PRIMARY KEY (id_cancer, id_treat)";
+					+ " PRIMARY KEY (id_cancer, id_treat ))";
 			stmt1.executeUpdate(sql1);
 			
 			// Create table symptoms
 			sql1 = "CREATE TABLE symptomps " 
 					+ "(id_symp  INTEGER  PRIMARY KEY AUTOINCREMENT,"
-					+ " detail    TEXT     NOT NULL, )";
+					+ " detail    TEXT     NOT NULL )";
 			stmt1.executeUpdate(sql1);
 			
 			// Create table treatment
@@ -95,14 +96,14 @@ public class SQLMaster implements DBMaster {
 					+ "(id_treat   INTEGER  PRIMARY KEY AUTOINCREMENT,"
 					+ " type    TEXT     NOT NULL, "
 					+ " startdate     DATE NOT NULL, "
-					+ " enddate DATE NOT NULL  TEXT, )";
+					+ " enddate DATE NOT NULL )";
 			stmt1.executeUpdate(sql1);
 			
 			// Create table patient_symptoms
 			sql1 = "CREATE TABLE patient_symptoms"
 					+ "(id_patient INTEGER REFERENCES patient(id_patient) ON DELETE SET NULL, "
 					+ " id_symp INTEGER REFERENCES symptoms(id_symp) ON DELETE SET NULL, "
-					+ " PRIMARY KEY (id_patient, id_symp))";
+					+ " PRIMARY KEY (id_patient, id_symp) )";
 
 			stmt1.executeUpdate(sql1);
 			
@@ -110,7 +111,7 @@ public class SQLMaster implements DBMaster {
 			sql1 = "CREATE TABLE family_history "
 					+  "(id_famHistory INTEGER PRIMARY KEY AUTOINCREMENT, "
 					+  "  type TEXT, "
-					+  "  member TEXT, )";
+					+  "  member TEXT )";
 					
 			stmt1.executeUpdate(sql1);
 			
@@ -119,14 +120,14 @@ public class SQLMaster implements DBMaster {
 			sql1 = "CREATE TABLE cancer_patient "
 					+  "(id_cancer INTEGER REFERENCES cancer (id_cancer), "
 					+  " id_patient INTEGER REFERENCES patient (id_patient), "
-					+  " PRIMARY KEY (id_cancer, id_patient), )";
+					+  " PRIMARY KEY (id_cancer, id_patient) )";
 					
 			stmt1.executeUpdate(sql1);
 			stmt1.close();
 		} catch (SQLException e) {
-			if (!e.getMessage().contains("already exists")) {
+			//if (!e.getMessage().contains("already exists")) {
 				e.printStackTrace();
-			}
+			//}
 		}
 
 	}
@@ -134,10 +135,16 @@ public class SQLMaster implements DBMaster {
 	@Override
 	public void addPatient(Patient p) {
 		try{
-			Statement stmt=c.createStatement();
-			String sql="INSERT INTO patient(name, surname) "
-					+ "VALUES('" + p.getName() + p.getSurname() +"')"; 
-			stmt.close();
+			String sql="INSERT INTO patient (name, surname, sex, date_birth, location, actual_state) "
+					+ "VALUES(?, ?, ?, ?)";
+			PreparedStatement prep=c.prepareStatement(sql);
+			prep.setString(1, p.getName());
+			prep.setString(2, p.getSurname());
+			prep.setString(3,p.getSex());
+			prep.setDate(4, p.getDate());
+			prep.ser
+			prep.executeUpdate();
+			prep.close();
 			}
 			catch(Exception e){
 			e.printStackTrace();
@@ -149,7 +156,9 @@ public class SQLMaster implements DBMaster {
 	
 	public List<Patient> searchPatientByName (String name,String surname) { 
 		// TODO Unsafe method, update later
-		// TODO What happens if name is null?
+		if(name==null) {
+			name="";
+		}
 		List<Patient> patient_list = new ArrayList<Patient>();
 		try {
 			Statement stmt = c.createStatement();
@@ -174,30 +183,19 @@ public class SQLMaster implements DBMaster {
 	
 	
 	
-	public List<Patient> removePatientByName(String name,String surname) {
+	public void removePatient(int patient_id) {
 		// TODO Unsafe method, update later
-		// TODO What happens if name is null?
-		List<Patient> patient_list = new ArrayList<Patient>();
 		try {
-			Statement stmt = c.createStatement();
-			String sql = "SELECT * FROM patient WHERE name,surname "
-					+ " LIKE '%" + name + "% ' ,'%"+ surname + "%'" ;
-			ResultSet rs = stmt.executeQuery(sql);
-			while (rs.next()) { // true: there is another result and I have advanced to it
-								// false: there are no more results
-				int id = rs.getInt("id");
-				String patientName = rs.getString("name");
-				String patientSurname=rs.getString("surname");
-				Patient p = new Patient (id, patientName,patientSurname);
-				patient_list.remove(p);
-			}
-			rs.close();
-			stmt.close();
+			String sql = "DELETE FROM patient WHERE patient_id= ? " ;
+			PreparedStatement prep = c.prepareStatement(sql);
+			prep.setInt(1, patient_id);
+			prep.executeUpdate();
+			prep.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return patient_list;
 	}
+	
 	
 
 	@Override
@@ -226,6 +224,12 @@ public class SQLMaster implements DBMaster {
 
 	@Override
 	public MedicalExamination infoSymptoms(Symptoms s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Patient> removePatientByName(String name, String surname) {
 		// TODO Auto-generated method stub
 		return null;
 	}
