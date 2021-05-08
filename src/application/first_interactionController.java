c2cf321609388ca2f90f10e07ae859f68e8c26ea
Angela <agarciagrech@gmail.com>
2021-05-08 12:51:47 +0200

    Working on javafx

--- src/application/first_interactionController.java
+++ src/application/first_interactionController.java
@@ -1,20 +1,36 @@
 package application;
 
 import java.awt.TextField;
+
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.scene.Node;
+
 
 public class first_interactionController extends Application {
 	
-	@FXML private TextField D;
-	@FXML private TextField N;
-	@FXML private TextField P;
+	@FXML private Button D;
+	@FXML private Button N;
+	@FXML private Button P;
 	
 	@FXML
-    void accionD(ActionEvent event) {
+    void accionD(ActionEvent event) throws IOException {
+		Parent loginParent = FXMLLoader.load(getClass().getResource("login.fxml"));
+		Scene loginScene= new Scene(loginParent);
+		
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		
+		window.setScene(loginScene);
+		window.show();
+		
 		
 
     }
@@ -28,6 +44,12 @@ public class first_interactionController extends Application {
     void accionP(ActionEvent event) {
 
     }
+
+	public void start(String[] arg0) throws Exception {
+		// TODO Auto-generated method stub
+		launch(arg0);
+		
+	}
 
 	@Override
 	public void start(Stage arg0) throws Exception {
