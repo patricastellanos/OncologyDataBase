5fe61ad519bcf236fc3c6983ceb44888402e9967
Angela <agarciagrech@gmail.com>
2021-05-09 19:48:52 +0200

    java fx :(

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,15 +1,17 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
+	public void start(Stage primaryStage) throws Exception {
+		 try{
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
@@ -18,6 +20,17 @@ public class Main extends Application {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	
+		/*FXMLLoader loader = new FXMLLoader(getClass().getResource("/Users/agarc/git/OncologyDataBase/first_interaction.fxml"));
+		
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();*/
+		
+		
 	}
 	
 	public static void main(String[] args) {
