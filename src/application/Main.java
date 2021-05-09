ceed60a260330a7bd73f63d89dcc21d7bfb62d5b
Angela <agarciagrech@gmail.com>
2021-05-09 20:36:35 +0200

    :)

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,22 +1,43 @@
 package application;
 	
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import actions.SQLMaster;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import oncology.db.interfaces.DBMaster;
+import oncology.db.interfaces.UserMaster;
+import oncology.db.jpa.JPAUserMaster;
+import oncology.db.pojos.Patient;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
 
 
 public class Main extends Application {
+	
+	private static DBMaster dbmaster= new SQLMaster();
+	private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	private static UserMaster userman = new JPAUserMaster();
+	
 	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public void start(Stage firstStage) {
 		 try{
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			 dbmaster.connect();
+			
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("first_interaction.fxml"));
+			Scene scene = new Scene(root,500,500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			firstStage.setScene(scene);
+			firstStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
