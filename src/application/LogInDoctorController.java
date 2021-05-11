3be3cd92f968039ebb1f2ac2a1f5522a53100e19
Angela <agarciagrech@gmail.com>
2021-05-11 23:17:17 +0200

    changes in login method

--- src/application/LogInDoctorController.java
+++ src/application/LogInDoctorController.java
@@ -8,25 +8,34 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
+import javafx.stage.Window;
+import oncology.db.interfaces.UserMaster;
+import oncology.db.jpa.JPAUserMaster;
+import oncology.db.pojos.users.User;
 
 public class LogInDoctorController {
 	
 
-    public LogInDoctorController() {
+    /*public LogInDoctorController() {
 		
-	}
+	}*/
+	private static UserMaster userman = new JPAUserMaster();
 
 	@FXML
     private PasswordField passText;
 
     @FXML
     private TextField userText;
-
+    
     @FXML
+    private Button loginButton;
     void actionLogin(ActionEvent event) {
-    	if(userText.getText().equals("")&& passText.getText().equals("123")) {
+    	/*if(userText.getText().equals("")&& passText.getText().equals("123")) {
 			try {
 
 				Parent root = FXMLLoader.load(getClass().getResource("LoginDoctor.fxml"));
@@ -43,7 +52,26 @@ public class LogInDoctorController {
 				e.printStackTrace();
 			}
 
-		}
+		}*/
+    	
+    	Window owner = loginButton.getScene().getWindow();
+    	if (userText.getText().isEmpty()) {
+    		showAlert(Alert.AlertType.ERROR, owner,"Error!", "Please enter your email");
+    		return;
+    	}
+    	if (passText.getText().isEmpty()) {
+    		showAlert(Alert.AlertType.ERROR, owner,"Error!", "Please enter your password again");
+    		return;
+    	}
+    	String username = userText.getText();
+    	String password = passText.getText();
+    	
+       User doctor = userman.checkPassword(username, password);
+       if(doctor==null) {
+    	   infoMessage("Please enter correct username or password", null, "Failed");
+       }else {
+    	   infoMessage("Successfull log in !!", null, "Failed");
+       }
 
     }
 
@@ -51,6 +79,22 @@ public class LogInDoctorController {
     void actionPassword(ActionEvent event) {
     
 		}
+    
+    public static void infoMessage(String infoMessage, String headerText, String title) {
+    	Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setContentText(infoMessage);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.showAndWait();
+    }
+    public static void showAlert(Alert.AlertType alertType, Window owner, String title, String message ) {
+    	Alert alert = new Alert(alertType);
+    	alert.setTitle(title);
+    	alert.setHeaderText(null);
+    	alert.setContentText(message);
+    	alert.initOwner(owner);
+    	alert.show();
+    }
     
 
 }
