6199ae6181d19f8e691b6275590b55dfddcb1f2f
maria <maria@DESKTOP-P6GESHR.home>
2021-05-17 17:19:25 +0200

    Java Fx

---
+++ src/application/SearchPatientDoctorController.java
@@ -0,0 +1,33 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SearchPatientDoctorController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button searchP;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    void actionBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionExit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionSearch(ActionEvent event) {
+
+    }
+
+}
---
+++ src/application/SearchedPatientController.java
@@ -0,0 +1,74 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SearchedPatientController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private TextField nameP;
+
+    @FXML
+    private TextField surnameP;
+
+    @FXML
+    private TextField actualStateP;
+
+    @FXML
+    private TextField sexP;
+
+    @FXML
+    private TextField dobP;
+
+    @FXML
+    private TextField locationP;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    void actionActualStateP(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionDOB(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionExit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionLocation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionName(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionSex(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionSurname(ActionEvent event) {
+
+    }
+
+}
