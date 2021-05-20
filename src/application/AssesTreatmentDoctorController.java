0e69a64580f30ba41f9aaa9035aeb5dfea2a6dca
maria <maria@10.60.59.83>
2021-05-20 14:54:56 +0200

    Treatment files created and code almost done

---
+++ src/application/AssesTreatmentDoctorController.java
@@ -0,0 +1,70 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class AssesTreatmentDoctorController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private TextField id;
+
+    @FXML
+    private Button asses;
+
+    @FXML
+    private Button seeP;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    void actionAsses(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionBack(ActionEvent event) {
+    	try{
+			Parent root = FXMLLoader.load(getClass().getResource("TreatmentDoctor.fxml"));
+			Scene scene = new Scene(root);
+			Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+    }
+
+    @FXML
+    void actionExit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionSeeAllP(ActionEvent event) {
+    	try{
+			Parent root = FXMLLoader.load(getClass().getResource("SeeAllPatientsTreatmentDoctor.fxml"));
+			Scene scene = new Scene(root);
+			Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+    }
+
+}
