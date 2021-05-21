51a3e019f2486483f8364eabcd95eff3f02ab751
maria <maria@10.60.59.120>
2021-05-21 19:34:37 +0200

    Treatment :)

--- src/application/AskIDSeeTreatmentDoctorController.java
+++ src/application/AskIDSeeTreatmentDoctorController.java
@@ -2,8 +2,13 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class AskIDSeeTreatmentDoctorController {
 
@@ -24,6 +29,17 @@ public class AskIDSeeTreatmentDoctorController {
 
     @FXML
     void actionBack(ActionEvent event) {
+    	try{
+			Parent root = FXMLLoader.load(getClass().getResource("TreatmentDoctor.fxml"));
+			Scene scene = new Scene(root);
+			Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
 
     }
 
@@ -34,11 +50,23 @@ public class AskIDSeeTreatmentDoctorController {
 
     @FXML
     void actionExit(ActionEvent event) {
+    	System.exit(0);
 
     }
 
     @FXML
     void actionSeeAllP(ActionEvent event) {
+    	try{
+			Parent root = FXMLLoader.load(getClass().getResource("SeeAllPatientsTreatment.fxml"));
+			Scene scene = new Scene(root);
+			Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
 
     }
 
