946c145dc4b661c1e741f2523084bfcf07e1a9ba
patri <patri@10.60.58.116>
2021-05-21 20:08:30 +0200

    java fx

--- src/application/SearchPatientDoctorController.java
+++ src/application/SearchPatientDoctorController.java
@@ -71,32 +71,37 @@ public class SearchPatientDoctorController {
     	    	List<Patient> patients_list = db.searchPatientByName(name.getText(), surname.getText());
     	    	
     	    	
-    	    	tableselectedpatient.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-    	    	
-    	    	TableColumn<Patient, String> idCol = new TableColumn<>("ID");
-    	    	TableColumn<Patient, String> nameCol = new TableColumn<>("Name");
-    	    	TableColumn<Patient, String> surNameCol = new TableColumn<>("SurName");
-    	    	TableColumn<Patient, String> doBCol = new TableColumn<>("Date of birth");
-    	    	TableColumn<Patient, String> sexCol = new TableColumn<>("Sex");
-    	    	TableColumn<Patient, String> locationCol = new TableColumn<>("Location");
-    	    	TableColumn<Patient, String> actualStateCol = new TableColumn<>("Actual State");
-    	    	
-    	    	idCol.setCellValueFactory(data -> new SimpleStringProperty(Integer.toString(data.getValue().getId_patient())));
-    	    	nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
-    	    	surNameCol.setCellValueFactory(new PropertyValueFactory<>("surname"));
-    	    	DateFormat dateformat=new SimpleDateFormat("yyyy-mm-dd");
-    	    	doBCol.setCellValueFactory(data -> new SimpleStringProperty(dateformat.format(data.getValue().getDate_birth())));
-    	    	sexCol.setCellValueFactory(new PropertyValueFactory<>("sex"));
-    	    	locationCol.setCellValueFactory(new PropertyValueFactory<>("location"));
-    	    	actualStateCol.setCellValueFactory(new PropertyValueFactory<>("actual_state"));
-    	    	
-    	    	
-    	    	tableselectedpatient.getColumns().addAll( idCol, nameCol, surNameCol, doBCol, sexCol, locationCol, actualStateCol);
     	        //tableP.getItems().add(new Patient(1,"pepe", "perez", "male", new Date(1935, 05, 06),"home","complex_care"));
     	    	tableselectedpatient.getItems().addAll(patients_list);
     	        //System.out.println("Nombre primer paciente" + patients_list.get(0).getName());
+    	    	
+    	    	
     	    
 
+    }
+    public void initialize() {
+
+    	tableselectedpatient.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    	
+    	TableColumn<Patient, String> idCol = new TableColumn<>("ID");
+    	TableColumn<Patient, String> nameCol = new TableColumn<>("Name");
+    	TableColumn<Patient, String> surNameCol = new TableColumn<>("SurName");
+    	TableColumn<Patient, String> doBCol = new TableColumn<>("Date of birth");
+    	TableColumn<Patient, String> sexCol = new TableColumn<>("Sex");
+    	TableColumn<Patient, String> locationCol = new TableColumn<>("Location");
+    	TableColumn<Patient, String> actualStateCol = new TableColumn<>("Actual State");
+    	
+    	idCol.setCellValueFactory(data -> new SimpleStringProperty(Integer.toString(data.getValue().getId_patient())));
+    	nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+    	surNameCol.setCellValueFactory(new PropertyValueFactory<>("surname"));
+    	DateFormat dateformat=new SimpleDateFormat("yyyy-mm-dd");
+    	doBCol.setCellValueFactory(data -> new SimpleStringProperty(dateformat.format(data.getValue().getDate_birth())));
+    	sexCol.setCellValueFactory(new PropertyValueFactory<>("sex"));
+    	locationCol.setCellValueFactory(new PropertyValueFactory<>("location"));
+    	actualStateCol.setCellValueFactory(new PropertyValueFactory<>("actual_state"));
+    	
+    	
+    	tableselectedpatient.getColumns().addAll( idCol, nameCol, surNameCol, doBCol, sexCol, locationCol, actualStateCol);
     }
 
 }
