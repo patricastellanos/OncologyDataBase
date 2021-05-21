f37ec11144feb699d8a2918598fb7d8b4252d714
patri <patri@10.60.58.116>
2021-05-21 19:43:57 +0200

    Search patient java fx done

--- src/application/SearchPatientDoctorController.java
+++ src/application/SearchPatientDoctorController.java
@@ -1,7 +1,12 @@
 package application;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
 import com.gluonhq.charm.glisten.control.TextField;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,10 +14,15 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import oncology.db.interfaces.DBMaster;
+import oncology.db.pojos.Patient;
 
 public class SearchPatientDoctorController {
+	private DBMaster db = Main.getdbMaster();
 
     @FXML
     private Button backButton;
@@ -27,7 +37,7 @@ public class SearchPatientDoctorController {
     private Button searchP;
 
     @FXML
-    private TableView<?> tableselectedpatient;
+    private TableView<Patient> tableselectedpatient;
 
     @FXML
     private Button exitButton;
@@ -55,7 +65,37 @@ public class SearchPatientDoctorController {
 
     @FXML
     void actionSearch(ActionEvent event) {
-    	
+    	  
+    	    
+    	    	
+    	    	List<Patient> patients_list = db.searchPatientByName(name.getText(), surname.getText());
+    	    	
+    	    	
+    	    	tableselectedpatient.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    	    	
+    	    	TableColumn<Patient, String> idCol = new TableColumn<>("ID");
+    	    	TableColumn<Patient, String> nameCol = new TableColumn<>("Name");
+    	    	TableColumn<Patient, String> surNameCol = new TableColumn<>("SurName");
+    	    	TableColumn<Patient, String> doBCol = new TableColumn<>("Date of birth");
+    	    	TableColumn<Patient, String> sexCol = new TableColumn<>("Sex");
+    	    	TableColumn<Patient, String> locationCol = new TableColumn<>("Location");
+    	    	TableColumn<Patient, String> actualStateCol = new TableColumn<>("Actual State");
+    	    	
+    	    	idCol.setCellValueFactory(data -> new SimpleStringProperty(Integer.toString(data.getValue().getId_patient())));
+    	    	nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+    	    	surNameCol.setCellValueFactory(new PropertyValueFactory<>("surname"));
+    	    	DateFormat dateformat=new SimpleDateFormat("yyyy-mm-dd");
+    	    	doBCol.setCellValueFactory(data -> new SimpleStringProperty(dateformat.format(data.getValue().getDate_birth())));
+    	    	sexCol.setCellValueFactory(new PropertyValueFactory<>("sex"));
+    	    	locationCol.setCellValueFactory(new PropertyValueFactory<>("location"));
+    	    	actualStateCol.setCellValueFactory(new PropertyValueFactory<>("actual_state"));
+    	    	
+    	    	
+    	    	tableselectedpatient.getColumns().addAll( idCol, nameCol, surNameCol, doBCol, sexCol, locationCol, actualStateCol);
+    	        //tableP.getItems().add(new Patient(1,"pepe", "perez", "male", new Date(1935, 05, 06),"home","complex_care"));
+    	    	tableselectedpatient.getItems().addAll(patients_list);
+    	        //System.out.println("Nombre primer paciente" + patients_list.get(0).getName());
+    	    
 
     }
 
