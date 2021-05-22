bfde9e043c9ca60eb43dad8feabea625c6d87689
maria <maria@DESKTOP-P6GESHR.home>
2021-05-22 13:06:55 +0200

    images

--- src/application/SeeSymptomsDoctorController.java
+++ src/application/SeeSymptomsDoctorController.java
@@ -1,60 +1,42 @@
-package application;
+papackage application;
 
 import javafx.event.ActionEvent;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.List;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import oncology.db.interfaces.DBMaster;
-import oncology.db.pojos.Symptoms;
 
 public class SeeSymptomsDoctorController {
 
     @FXML
-    private TableView<Symptoms> tableP;
+    private Button backButton;
+
+    @FXML
+    private TextField id;
+
+    @FXML
+    private Button add;
+
+    @FXML
+    private Button seeP;
 
     @FXML
     private Button exitButton;
 
     @FXML
-    private Button backButton;
-    
-    private DBMaster db = Main.getdbMaster();
-    
-public void initialize(){
-    	
-    	List<Symptoms> sympList = db.printPatientSymptoms(0);
-    	
-    	
-    	tableP.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-    	
-    	TableColumn<Symptoms, String> idCol = new TableColumn<>("ID");
-    	TableColumn<Symptoms, String> detailsCol = new TableColumn<>("Details");
-    	
-    	idCol.setCellValueFactory(data -> new SimpleStringProperty(Integer.toString(data.getValue().getId_symp())));
-    	detailsCol.setCellValueFactory(new PropertyValueFactory<>("details"));
-    	
-    	
-    	tableP.getColumns().addAll(idCol, detailsCol);
-        tableP.getItems().addAll(sympList);
-        
-    }
+    void actionAdd(ActionEvent event) {
 
+    }
 
     @FXML
     void actionBack(ActionEvent event) {
     	try{
-			Parent root = FXMLLoader.load(getClass().getResource("AskIDSymptomsDoctor.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("SymptomsDoctor.fxml"));
 			Scene scene = new Scene(root);
 			Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		
@@ -64,7 +46,6 @@ public void initialize(){
 			e.printStackTrace();
 		}
 
-
     }
 
     @FXML
@@ -73,5 +54,26 @@ public void initialize(){
 
     }
 
+    @FXML
+    void actionSeeAllP(ActionEvent event) {
+    	try{
+			Parent root = FXMLLoader.load(getClass().getResource("SeeAllPatientsSymptomsDoctor.fxml"));
+			Scene scene = new Scene(root);
+			Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+    }
+    
+    int idSymp =id.getAnchor();
+
+    public void setData(int idSymp ) {
+        target.setText(data);
+
 }
+
 
