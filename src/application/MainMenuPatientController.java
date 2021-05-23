25870efe572cb13941e069959a653a9535f7f671
maria <maria@DESKTOP-P6GESHR>
2021-05-23 20:30:37 +0200

    patient

---
+++ src/application/MainMenuPatientController.java
@@ -0,0 +1,52 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MainMenuPatientController {
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    void actionBack(ActionEvent event) {
+    	try{
+			Parent root = FXMLLoader.load(getClass().getResource("LogInPatient.fxml"));
+			Scene scene = new Scene(root);
+			Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+
+    }
+
+    @FXML
+    void actionExit(ActionEvent event) {
+    	System.exit(0);
+
+    }
+
+    @FXML
+    void actionSeeFamilyHistory(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionSeeMedicalExamination(ActionEvent event) {
+
+    }
+
+}
