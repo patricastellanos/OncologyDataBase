345291af54cbdea93745c94f46094710bc7dd14f
patri <patri@DESKTOP-A47KS9S>
2021-06-04 15:55:41 +0200

    Mistakes Doctor fixed

--- src/application/SeeMedicalExaminationDoctorController.java
+++ src/application/SeeMedicalExaminationDoctorController.java
@@ -15,7 +15,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import com.gluonhq.charm.glisten.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import oncology.db.interfaces.DBMaster;
